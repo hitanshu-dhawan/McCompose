2b0093b50a00df47f9be3ac51e2e16ba6587ca02
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-10-09 03:23:51 +0530

    :recycle: Rearrange code

--- app/src/main/java/com/hitanshudhawan/mccompose/data/CategoriesRepository.kt
+++ app/src/main/java/com/hitanshudhawan/mccompose/data/CategoriesRepository.kt
@@ -8,33 +8,33 @@ object CategoriesRepository {
         return listOf(
             Category(
                 id = 1,
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "quarter_pounder_with_cheese_deluxe.png",
                 name = "Burgers",
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "quarter_pounder_with_cheese_deluxe.png",
             ),
             Category(
                 id = 2,
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "medium_french_fries.png",
                 name = "Fries",
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "medium_french_fries.png",
             ),
             Category(
                 id = 3,
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "coca_cola.png",
                 name = "Beverages",
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "coca_cola.png",
             ),
             Category(
                 id = 4,
+                name = "Combo Meals",
                 image = "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/meal/desktop/h-mcdonalds-2-Cheeseburger-Extra-Value-Meals.jpg",
-                name = "Combo Meals"
             ),
             Category(
                 id = 5,
-                image = "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/meal/desktop/h-mcdonalds-Hamburger.png",
                 name = "Happy Meals",
+                image = "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/meal/desktop/h-mcdonalds-Hamburger.png",
             ),
             Category(
                 id = 6,
-                image = "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Vanilla-Reduced-Fat-Ice-Cream-Cone.jpg",
                 name = "Desserts",
+                image = "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Vanilla-Reduced-Fat-Ice-Cream-Cone.jpg",
             )
         )
     }
