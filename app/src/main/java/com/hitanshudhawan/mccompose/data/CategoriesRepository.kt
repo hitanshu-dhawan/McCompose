f7a9ee6aadd5efe9e4113a995bfc3abb303f83e5
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-10-10 03:47:27 +0530

    :lipstick: SpotlightCard new improved UI

--- app/src/main/java/com/hitanshudhawan/mccompose/data/CategoriesRepository.kt
+++ app/src/main/java/com/hitanshudhawan/mccompose/data/CategoriesRepository.kt
@@ -9,32 +9,32 @@ object CategoriesRepository {
             Category(
                 id = 1,
                 name = "Burgers",
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "quarter_pounder_with_cheese_deluxe.png"
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/Budgety/master/images/" + "quarter_pounder_with_cheese_deluxe.png"
             ),
             Category(
                 id = 2,
                 name = "Fries",
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "medium_french_fries.png"
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/Budgety/master/images/" + "medium_french_fries.png"
             ),
             Category(
                 id = 3,
                 name = "Beverages",
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "coca_cola.png"
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/Budgety/master/images/" + "coca_cola.png"
             ),
             Category(
                 id = 4,
                 name = "Combo Meals",
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "cheeseburger_combo_meal.png"
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/Budgety/master/images/" + "cheeseburger_combo_meal.png"
             ),
             Category(
                 id = 5,
                 name = "Happy Meals",
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "hamburger_happy_meal.png"
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/Budgety/master/images/" + "hamburger_happy_meal.png"
             ),
             Category(
                 id = 6,
                 name = "Desserts",
-                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/PigGame/master/images/" + "vanilla_cone.png"
+                image = "https://raw.githubusercontent.com/DumbPolyglotProgrammer/Budgety/master/images/" + "vanilla_cone.png"
             )
         )
     }
--- app/src/main/java/com/hitanshudhawan/mccompose/ui/components/SpotlightCard.kt
+++ app/src/main/java/com/hitanshudhawan/mccompose/ui/components/SpotlightCard.kt
@@ -4,13 +4,11 @@ import androidx.compose.foundation.Box
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Layout
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -29,22 +27,9 @@ fun SpotlightCard(
     ) {
         Column(
             modifier = Modifier
-                .clickable(onClick = { /*todo*/ })
                 .fillMaxWidth()
-                .padding(vertical = 32.dp),
-            horizontalGravity = Alignment.CenterHorizontally
+                .clickable(onClick = { /*todo*/ })
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1.75f),
-            ) {
-                CoilImage(
-                    data = imageUrl,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier.fillMaxSize(),
-                )
-            }
             Spacer(modifier = Modifier.preferredHeight(16.dp))
             Text(
                 text = title,
@@ -53,6 +38,37 @@ fun SpotlightCard(
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(horizontal = 16.dp)
             )
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1.00f),
+            ) {
+                Layout(
+                    children = {
+                        CoilImage(
+                            data = imageUrl,
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier
+                                .wrapContentWidth()
+                                .fillMaxHeight()
+                        )
+                    }
+                ) { measurables, constraints ->
+
+                    val imagePlaceable = measurables[0].measure(constraints)
+
+                    layout(
+                        width = constraints.maxWidth,
+                        height = constraints.minHeight
+                    ) {
+                        imagePlaceable.place(
+                            x = (constraints.maxWidth - imagePlaceable.width * 0.75).toInt(),
+                            y = 0
+                        )
+                    }
+                }
+            }
         }
     }
 }
