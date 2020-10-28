a919b194498d5e16502bb3e01436dbf7079d76f4
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-10-29 02:49:39 +0530

    :building_construction: Dependency Management using buildSrc

---
+++ buildSrc/src/main/java/com/hitanshudhawan/mccompose/buildsrc/Dependencies.kt
@@ -0,0 +1,52 @@
+package com.hitanshudhawan.mccompose.buildsrc
+
+object Libs {
+
+    const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha13"
+
+    object Accompanist {
+        private const val version = "0.2.1"
+        const val coil = "dev.chrisbanes.accompanist:accompanist-coil:$version"
+    }
+
+    object Kotlin {
+        private const val version = "1.4.10"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object AndroidX {
+
+        const val material = "com.google.android.material:material:1.2.0"
+
+        object Compose {
+            private const val version = "1.0.0-alpha05"
+
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+            const val ui = "androidx.compose.ui:ui:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val materialIconsExtended = "androidx.compose.material:material-icons-extended:$version"
+            const val animation = "androidx.compose.animation:animation:$version"
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val runtimeLiveData = "androidx.compose.runtime:runtime-livedata:$version"
+            const val tooling = "androidx.ui:ui-tooling:$version"
+            const val test = "androidx.compose.test:test-core:$version"
+            const val uiTest = "androidx.ui:ui-test:$version"
+        }
+
+        object Test {
+            private const val version = "1.2.0"
+            const val core = "androidx.test:core:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            object Ext {
+                private const val version = "1.1.2-rc01"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+    }
+}
